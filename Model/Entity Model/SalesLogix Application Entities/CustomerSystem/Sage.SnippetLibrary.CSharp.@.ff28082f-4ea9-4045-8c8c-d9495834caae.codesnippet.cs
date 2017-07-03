d83434d3ce99e166d5b6d37d6845709f2cd73067
Dan Ramsey <dan.ramsey@customerfx.com>
2017-07-03 18:53:50 +0000

    Added contact to customer system and created bundle

---
+++ Model/Entity Model/SalesLogix Application Entities/CustomerSystem/Sage.SnippetLibrary.CSharp.@.ff28082f-4ea9-4045-8c8c-d9495834caae.codesnippet.cs
@@ -0,0 +1,43 @@
+/*
+ * This metadata is used by the Saleslogix platform.  Do not remove.
+<snippetHeader xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" id="ff28082f-4ea9-4045-8c8c-d9495834caae">
+ <assembly>Sage.SnippetLibrary.CSharp</assembly>
+ <name>OnCreateStep</name>
+ <references>
+  <reference>
+   <assemblyName>Sage.Entity.Interfaces.dll</assemblyName>
+   <hintPath>%BASEBUILDPATH%\interfaces\bin\Sage.Entity.Interfaces.dll</hintPath>
+  </reference>
+  <reference>
+   <assemblyName>Sage.Form.Interfaces.dll</assemblyName>
+   <hintPath>%BASEBUILDPATH%\formInterfaces\bin\Sage.Form.Interfaces.dll</hintPath>
+  </reference>
+  <reference>
+   <assemblyName>Sage.Platform.dll</assemblyName>
+   <hintPath>%BASEBUILDPATH%\assemblies\Sage.Platform.dll</hintPath>
+  </reference>
+  <reference>
+   <assemblyName>Sage.SalesLogix.API.dll</assemblyName>
+  </reference>
+ </references>
+</snippetHeader>
+*/
+
+
+#region Usings
+using System;
+using Sage.Entity.Interfaces;
+using Sage.Form.Interfaces;
+using Sage.SalesLogix.API;
+#endregion Usings
+
+namespace Sage.BusinessRules.CodeSnippets
+{
+    public static partial class CustomerSystemBusinessRules
+    {
+        public static void OnCreateStep( ICustomerSystem customersystem)
+        {
+            customersystem.ValidDate = DateTime.Now;
+        }
+    }
+}
